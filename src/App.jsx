9a85e11c4f1f9abf4042405d5348f275eba8bcfa
Eng.Tala <talabts33@gmail.com>
2026-06-20 15:56:08 +0300

    feat: link registration page to login and login to registration + add email verification success page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import EmailVerificationSuccess from "./pages/EmailVerificationSuccess";
+import RegistrationPage from "./pages/RegistrationPage";
+
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
         <Route path="/sponsor-dashboard" element={<h1>Sponsor Dashboard</h1>} />
         <Route path="/guardian-dashboard" element={<h1>Guardian Dashboard</h1>} />
         <Route path="/admin-dashboard" element={<h1>Admin Dashboard</h1>} />
+        <Route path="/email-verified" element={<EmailVerificationSuccess />} />
+        <Route path="/register" element={<RegistrationPage />} />
+
       </Routes>
     </BrowserRouter>
   );
