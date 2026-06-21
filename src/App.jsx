23bd0a0e070ccc0a04731f3363b42d868688d4e2
ahmadshima29 <ahmadshima29@gmail.com>
2026-06-22 01:36:08 +0300

    feat: build reset password page UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,61 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// // import { BrowserRouter, Routes, Route } from "react-router-dom";
+// // import Login from "./pages/Login";
+
+// // function App() {
+// //   return (
+// //     <BrowserRouter>
+// //       <Routes>
+// //         <Route path="/" element={<Login />} />
+// //         <Route path="/sponsor-dashboard" element={<h1>Sponsor Dashboard</h1>} />
+// //         <Route path="/guardian-dashboard" element={<h1>Guardian Dashboard</h1>} />
+// //         <Route path="/admin-dashboard" element={<h1>Admin Dashboard</h1>} />
+// //       </Routes>
+// //     </BrowserRouter>
+// //   );
+// // }
+
+// // export default App;
+// // import { Routes, Route } from "react-router-dom";
+// // import ResetPassword from "./pages/ResetPassword";
+// // import Login from "./pages/Login";
+
+// // function App() {
+// //   return (
+// //     <Routes>
+// //         <Route path="/reset-password" element={<ResetPassword />} />
+        
+// //      </Routes>
+     
+// //   );
+// // }
+
+// // export default App;
+// import { Routes, Route, Navigate } from "react-router-dom";
+// import ResetPassword from "./pages/ResetPassword";
+// // import Login from "./pages/Login";
+
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Navigate to="/reset-password" />} />
+//       <Route path="/reset-password" element={<ResetPassword />} />
+//        <Route path="/login" element={<Login />} /> 
+//     </Routes>
+//   );
+// }
+
+// export default App;
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
+import ResetPassword from "./pages/ResetPassword";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/sponsor-dashboard" element={<h1>Sponsor Dashboard</h1>} />
-        <Route path="/guardian-dashboard" element={<h1>Guardian Dashboard</h1>} />
-        <Route path="/admin-dashboard" element={<h1>Admin Dashboard</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/reset-password" />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
