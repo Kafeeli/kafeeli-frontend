548cd6249304165696ecbe49a2aba0c9746c3562
Eng.Tala <talabts33@gmail.com>
2026-07-22 14:25:31 +0300

    edit logo in side bar

--- src/pages/guardian-dashboard/Sidebar.jsx
+++ src/pages/guardian-dashboard/Sidebar.jsx
@@ -135,13 +135,15 @@ function Sidebar({ openSidebar, setOpenSidebar }) {
         {/* Logo */}
         <div className="flex flex-col items-center justify-center px-4 py-3 shrink-0">
           <div className="text-center mb-2">
-            <div className="w-24 h-24 bg-white/20 rounded-full mx-auto mb-2 flex items-center justify-center overflow-hidden">
-              <img
-                src={kafeeliLogo}
-                alt="كفيلي"
-                className="w-30 h-30 object-contain mt-1"
-              />
-            </div>
+            <div className="text-center mb-2">
+          <div className="w-[100px] h-[100px] bg-white/20 rounded-full mx-auto mb-2 flex items-center justify-center overflow-hidden">
+            <img
+              src="/src/assets/title.png"
+              alt="كفيلي"
+              className="w[140px] h-[140px] object-contain mt-2"
+            />
+          </div>
+        </div>
           </div>
 
           <h2 className="font-[Cairo] font-bold text-[18px] sm:text-[20px] lg:text-[22px] tracking-[0px] text-center text-[#FFDEAA] mb-2">
