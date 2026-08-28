e6ca15f2b1a05234027cbcd18f9a58a25198e14b
Ameer Saqer <ameer0599333607@gmail.com>
2026-08-28 16:08:36 +0300

    updateSponsor

--- src/pages/sponsor-dashboard/SponsorFlowLayout.jsx
+++ src/pages/sponsor-dashboard/SponsorFlowLayout.jsx
@@ -3,15 +3,33 @@ import Sidebar from "./Sidebar";
 import AuthenticatedHeader from "../../components/layout/AuthenticatedHeader";
 import AuthenticatedFooter from "../../components/layout/AuthenticatedFooter";
 
-export default function SponsorFlowLayout({ title, description, backTo, children }) {
+export default function SponsorFlowLayout({
+  title,
+  description,
+  backTo,
+  children,
+}) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
     <div dir="rtl" className="flex min-h-screen bg-gray-50 font-[Cairo]">
-      <Sidebar openSidebar={isSidebarOpen} setOpenSidebar={setIsSidebarOpen} />
-      <div className="min-w-0 flex-1 lg:mr-64">
-        <AuthenticatedHeader title={title} description={description} backTo={backTo} onMenuClick={() => setIsSidebarOpen(true)} />
-        <main className="mx-auto max-w-6xl p-4 md:p-6">{children}</main>
+      <Sidebar
+        openSidebar={isSidebarOpen}
+        setOpenSidebar={setIsSidebarOpen}
+      />
+
+      <div className="flex min-h-screen min-w-0 flex-1 flex-col lg:mr-64">
+        <AuthenticatedHeader
+          title={title}
+          description={description}
+          backTo={backTo}
+          onMenuClick={() => setIsSidebarOpen(true)}
+        />
+
+        <main className="mx-auto w-full max-w-6xl flex-1 p-4 md:p-6">
+          {children}
+        </main>
+
         <AuthenticatedFooter />
       </div>
     </div>
